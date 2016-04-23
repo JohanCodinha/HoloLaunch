5e70a86b1f4bba2a6b51fbc72eec0032a3dab899
shash7 <shashwat.amin@yahoo.com>
2016-04-24 01:00:56 +1000

    zz

--- src/js/app.js
+++ src/js/app.js
@@ -29,28 +29,48 @@
 		
 		
 		views.Hologram = Backbone.View.extend({
+			appWidth : 0,
 			events : {
-				'click' : 'onClick' 
 			},
 			initialize : function() {
-				console.log(this);
 				$('html').addClass('fullscreen');
 				//_.bindAll(this, 'centerImage');
 				this.createSquare();
+				this.setupCanvas();
 			},
 			createSquare : function() {
 				var el = $(this.el);
-				var width = el.width();
-				var height = el.height();
-				var appWidth = 0;
-				if(width > height) {
-					appWidth = height;
-				} else {
-					appWidth = width;
-				}
-				el.append('<div class="view"></div>');
 			},
 			setupCanvas : function() {
+				$('.view.top').append('<canvas id="main" width="100" height="100"></canvas>');
+				$('#main').width($('.view').width()).height($('.view').height());
+				common.canvas = new createjs.Stage("main");
+				var circle = new createjs.Shape();
+				circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 20);
+				circle.x = 50;
+				circle.y = 75;
+				common.canvas.addChild(circle);
+				circle = new createjs.Shape();
+				circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 20);
+				circle.x = 0;
+				circle.y = 25;
+				common.canvas.addChild(circle);
+				common.canvas.update();
+				this.setupSlaves();
+				setInterval(function() {
+
+				}, 10);
+			},
+			setupSlaves : function() {
+				$('.view.slave').each(function() {
+					var el = $(this);
+					var id = _.uniqueId('canvas_');
+					el.append('<canvas class="' + id + '" width="100" height="100"></canvas>');
+					$('.' + id).width($('.view').width()).height($('.view').height());
+					var c = document.getElementsByClassName(id)[0];
+					c = c.getContext('2d');
+					c.drawImage(common.canvas.canvas, 0, 0);
+				});
 			}
 			
 		});
@@ -65,7 +85,7 @@
 			loadViews();
 			
 			common.hologram = new views.Hologram({
-				el : $('.app')
+				el : $('.cube')
 			});
 			
 /*			$(window).resize(function() {
